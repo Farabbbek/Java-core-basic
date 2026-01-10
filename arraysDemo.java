7385ab73a3f69b16d587f13b311acb4f3afc9167
Al-Farabi <farabi@Al-Farabis-MacBook-Air.local>
2026-01-11 03:25:31 +0500

    feat: arraysDemo.java

---
+++ arraysDemo.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class ArraysDemo{
+    public static void main(String[] args){
+        String[] cars = {"Volvo","Toyota","BMW","Audi","Mersedes"};
+        System.out.println(Arrays.toString(cars));
+    }
+}
