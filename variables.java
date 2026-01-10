2f1af39b08fb6bc8a1196ebeea89f9c609ee7d13
Al-Farabi <farabi@Al-Farabis-MacBook-Air.local>
2026-01-10 20:15:26 +0500

    feat: variables.java file

---
+++ variables.java
@@ -0,0 +1,31 @@
+public class variables {
+    public static void main(String[] args) {
+        int LivingRoom = 20;
+        int BathRoom = 15;
+        int Kitchen = 2;
+
+        System.out.println(LivingRoom);
+        System.out.println(BathRoom);
+        System.out.println(Kitchen);
+        System.out.println("Variable integer");
+
+        String name = "\nFarabi";
+        String she = "Sabrina Carpenter";
+        String city = "Astana";
+        char grade = '#';
+        int age = 20;
+        boolean isStudent = true;
+        double gpa = 2.88;
+
+
+        System.out.println(name);
+        System.out.println(she);
+        System.out.println(city);
+        System.out.println(grade);
+        System.out.println(age);
+        System.out.println(isStudent);
+        System.out.println(gpa);
+        System.out.println("Variable different types");
+    }
+}
+
