524c6037440d1d7f1e9c9d9b5cd072a00666a5e2
Al-Farabi <farabi@Al-Farabis-MacBook-Air.local>
2026-01-10 20:25:07 +0500

    feat: operators.java file

---
+++ operators.java
@@ -0,0 +1,27 @@
+public class operators {
+    public static void main(String[] args){
+        int a = 15;
+        int b = 20;
+
+        //Addition
+        int sum = a + b;
+        System.out.println("Addition: " + sum);
+
+        //Subtraction
+        int difference = b - a;
+        System.out.println("Subtraction: " + difference);
+
+        //Multiplication
+        int multiplication = a * b;
+        System.out.println("Multiplication: " + multiplication);
+
+        //Division
+        int division = b / a;
+        System.out.println("Division: " + division);
+
+        //Modulus
+        int modulus = b % a;
+        System.out.println("Modulus: "+ modulus);
+
+    }
+}
