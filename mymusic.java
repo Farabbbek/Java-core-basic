20b3775d27073e8db1275bac11e462b623fcf40d
Al-Farabi <farabi@Al-Farabis-MacBook-Air.local>
2026-01-10 20:00:39 +0500

    feat: mymusic.java file

---
+++ mymusic.java
@@ -0,0 +1,11 @@
+public class mymusic {
+    public static void main(String[] args){
+        System.out.println("========== 🎵 Music  🎵 ==========");
+        System.out.println("JЫN ҰRADЫ"); // NAZZARBECK
+        System.out.println("ALGABAS");// De lacure
+        System.out.println("See You Again"); //Tyler, The Creator
+        System.out.println("Moparebit"); //TamAda 
+        System.out.println("Tunin Bir Uagynda"); //Almas
+        System.out.println("========== 🎵 Music  🎵 ==========");
+    }    
+}
