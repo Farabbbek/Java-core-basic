e57d2c707b17cdfa5266ddcd17caddd8107432ea
Al-Farabi <farabi@Al-Farabis-MacBook-Air.local>
2026-01-11 03:33:18 +0500

    feat: Indexing.java

---
+++ Indexing.java
@@ -0,0 +1,10 @@
+public class Indexing {
+    public static void main(String[] args){
+        String[] cars = {"Volvo","Toyota","BMW","Audi","Mersedes"};
+        System.out.println(cars[0]);
+        System.out.println(cars[1]);
+        System.out.println(cars[2]);
+        System.out.println(cars[3]);
+        System.out.println(cars[4]);
+    }
+}
